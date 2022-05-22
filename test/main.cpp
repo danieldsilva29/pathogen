b96621cbb463f90b3f1638f86011f895a821bcac
Eshaan Barkataki <eshaanbarkataki@gmail.com>
2022-05-21 21:47:53 -0400

    fixed stuff

--- test/main.cpp
+++ test/main.cpp
@@ -7,14 +7,28 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     App app = App("YOU LITTLE PERSON", 1000, 1000);
+    
+    auto texture = app.addTexture("AnotherImage.jpeg");
+    auto texture2 = app.addTexture("AnotherImage.jpeg");
     auto text = app.addText("HI BESHAN", "./font.ttf", 50, 255, 255, 255); 
-    auto text = app.addTexture("AnotherImage.jpeg");
 
     // adjust height and width of our image box.
+
+    texture->x = 800;
+    texture->y = 0;
+    texture->w = 200;
+    texture->h = 800;
+
+    texture2->x = 200;
+    texture2->y = 0;
+    texture2->w = 200;
+    texture2->h = 800;
+    
     text->x = 200;
-    text->y = 1000;
+    text->y = 500;
     text->w = 800;
     text->h = 200;
+
     int rot = 0;
     int close = 0;
     while (!close) {
